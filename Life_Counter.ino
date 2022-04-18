c748b524690fe9dde9f32969bf615a92601dc71d
dementeddr <2413901+dementeddr@users.noreply.github.com>
2022-04-17 22:57:55 -0400

    Adding reset functionality. Not yet functional

--- Life_Counter.ino
+++ Life_Counter.ino
@@ -4,9 +4,11 @@
 
 #define rotA 2
 #define rotB 3
-#define rotButt 10
-#define dnButt 11
-#define upButt 12
+#define rotButt 11
+#define dnButt 17
+#define upButt 16
+#define homeButt 15
+#define resetButt 14
 #define steps 24
 #define startLife 40
 #define lifeLen 3
@@ -17,8 +19,13 @@ bool render = true;
 int lCursor = 0;
 bool upLast = HIGH;
 bool dnLast = HIGH;
+bool homeLast = HIGH;
+bool resetLast = HIGH;
+long rotPos =  0;  
+int resetLen = 3000;
 
-volatile int lifes[4] = {startLife, startLife, startLife, startLife};
+
+volatile int lifes[4] = {startLife, 0, 0, 0};
 
 LinkedList<int> lHist[4]  = { 
     LinkedList<int>(),
@@ -33,13 +40,11 @@ void setup() {
   pinMode(rotButt, INPUT_PULLUP);
   pinMode(upButt, INPUT_PULLUP);
   pinMode(dnButt, INPUT_PULLUP);
+  pinMode(homeButt, INPUT_PULLUP);
+  pinMode(resetButt, INPUT_PULLUP);
 
-  for (int i=0; i<4; i++) {
-    lHist[i].add(lifes[i]);
-  }
+  counter_reset();
 }
-
-long rotPos =  0;
 
 void loop() {
   // put your main code here, to run repeatedly:
@@ -80,7 +85,16 @@ void loop() {
     dnLast = HIGH;
   }
 
-  
+  int resetTimer = resetLen;
+  while (digitalRead(resetButt) == LOW) {
+    if (resetTimer > 0) {
+      resetTimer = resetTimer - 10;
+      delay(10);
+    } else {
+      counter_reset();
+    }
+  }
+
   if (render) {
     screenWrite();
     render = false;
@@ -96,10 +110,22 @@ void screenWrite()
     String strout = " ";
     
     if (i == lCursor) {
-      strout = ">";
+      //strout = ">";
+      
+      switch (i) {
+        case 0: strout = "+"; break;
+        case 1: strout = "X"; break;
+        case 2: strout = "P"; break;
+        case 3: strout = "E"; break;
+      }
     }
     
     strout.concat(lifes[i]);
+
+    if (lifes[i] < 100 && lifes[i] > -10) {
+      strout.concat(' ');
+    }
+    
     strout.concat('|');
   
     for (int j=0; j<lHist[i].size()-1; j++) {
@@ -111,4 +137,16 @@ void screenWrite()
 
     lcd.print(strout.substring(0,20));
   }
+}
+
+void counter_reset() {
+
+  //lifes = {startLife, 0, 0, 0};
+
+  for (int i=0; i<4; i++) {
+    lHist[i].add(lifes[i]);
+  }
+
+  rotPos = 0;
+  render = true;
 }
